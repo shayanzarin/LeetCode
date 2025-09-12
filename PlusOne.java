63bd1a3162f77d3331ee6c2d82abf39cd6c4e0e5
shayanzarin <shayan.zarin.pars@gmail.com>
2025-09-12 17:20:19 +0200

    https://leetcode.com/problems/plus-one/description/

--- PlusOne.java
+++ PlusOne.java
@@ -1,41 +1,32 @@
 import java.util.Arrays;
 
 public class PlusOne {
-
     public int[] plusOne(int[] digits) {
-        int number = myNumber(digits) +1;
 
-        int myLength =0 ;
-        if (digits[digits.length-1] == 9) {
-            myLength = digits.length;
-        }else{
-            myLength = digits.length-1;
+        int lengh = digits.length;
+        for(int i=lengh-1; i>=0; i--){
+
+            if (digits[i] != 9) {
+                digits[i] ++;
+                return digits;
+            }
+            digits[i] = 0;
         }
 
-        
-        
-
-        return null;
-
-    }
-
-    public int myNumber(int [] digits){
-
-        int result =0 ;
-        int counter=0; 
-        for(int i=digits.length-1; i>=0; i--){
-            result += digits[i] * Math.pow(10, counter);
-            counter ++;
-        }
-
+        int[] result = new int[lengh+1];
+        result[0] = 1;
         return result;
+
     }
+
+
     public static void main(String[] args) {
 
         PlusOne po=new PlusOne();
-        int test[] = {1,3,4 };
+        int test[] = {9,8,7,6,5,4,3,2,1,0};
+
         System.out.println(Arrays.toString(po.plusOne(test)));
 
-        System.out.println(po.myNumber(test));
+
     }
 }
